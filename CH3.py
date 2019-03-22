93cf504982283ca65dc56001df59c5a8c37b68e0
GTVERZOSA <gtverzosa@gmail.com>
2019-03-22 13:06:14 -0700

    Chapter 3

---
+++ CH3.py
@@ -0,0 +1,41 @@
+fred = "Why do gorillas have big nostrils? Big fingers!!"
+print(fred)
+Why do gorillas have big nostrils? Big fingers!!
+fred = 'What is pink and fluffy? Pink fluff!!'
+print(fred)
+What is pink and fluffy? Pink fluff!!
+fred="How do dinosaurs pay their bills?
+SyntaxError: EOL while scanning string literal
+fred='''How do dinosaurs pay their bills?
+With tyrannosaurus checks!'''
+print(fred)
+How do dinosaurs pay their bills?
+With tyrannosaurus checks!
+silly_string='He said, "Aren't can't shouldn't wouldn't."'
+SyntaxError: invalid syntax
+silly_string="He said, "Aren't can't shouldn't wouldn't.""
+SyntaxError: invalid syntax
+silly_string='''He said "Aren't can't shouldn't wouldn't."'''
+single_quote_str='He saidm "Aren\'t can\'t shouldn\'t wouldn\'t."'
+double_quote_str="He said, \"Aren't can't shouldn't wouldn't.\""
+print(single_quote_str)
+He saidm "Aren't can't shouldn't wouldn't."
+print(double_quote_str)
+He said, "Aren't can't shouldn't wouldn't."
+myscore=1000
+message='I scored %s points'
+print(message%myscore)
+I scored 1000 points
+joke_text='%s: a device for finding furniture in the dark'
+bodypart1='Knee'
+bodypart2='Shin'
+print(joke_text%bodypart1)
+Knee: a device for finding furniture in the dark
+print(joke_text%bodypart2)
+Shin: a device for finding furniture in the dark
+nums='What did the number %s say to the number %s? Nice belt!!'
+print(nums%(0,8))
+What did the number 0 say to the number 8? Nice belt!!
+print(10*'a')
+aaaaaaaaaa
+ 
